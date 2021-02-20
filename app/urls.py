99e9854eb51745ee41bb354d14598afac53bb2b4
hosniaali <hosnia__ali@hotmail.com>
2021-02-20 10:00:37 +0300

    index

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,12 @@
-from django.contrib.auth import views
+# from django.contrib.auth import views
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from .api import *
-
+from .views import index
 
 urlpatterns = [
-    path('app/api/chatbot', ChatbotAPI.as_view()),
+                  path('', index, name='index'),
+                  path('app/api/chatbot', ChatbotAPI.as_view()),
 
-]
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
