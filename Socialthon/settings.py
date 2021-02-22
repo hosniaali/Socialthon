9f05e8fef8df200224522f4fb8fdaaf5546683f6
Abdulrahman <abdalrahman-talaat@hotmail.com>
2021-02-22 07:28:32 -0500

    to abdulrahman

--- Socialthon/settings.py
+++ Socialthon/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'pt*2j)-q*nkxuj=(jr1xpopac88)mtyx!mp%ae7)95b#+^^9gk'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -124,9 +124,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+STATIC_ROOT = '/home/selacxdj/public_html/static'
+
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
---
+++ passenger_wsgi.py
@@ -0,0 +1,1 @@
+from Socialthon.wsgi import application
